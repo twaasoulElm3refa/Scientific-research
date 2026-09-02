966bbcc05db6ae5240586efe85056ab354493053
twaasoulElm3refa <twaslm3rfa@gmail.com>
2026-09-02 11:57:02 +0300

    ..

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminUserSeeder::class,
-            UserSeeder::class,
+            userSeeder::class,
             CategorySeeder::class,
             CountrySeeder::class,
             LanguageSeeder::class,
